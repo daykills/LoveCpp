d815d6e971f009def14cb59435c1ca404ee9a23f
ZJ Yan <zengjia_yan@apple.com>
2019-03-26 23:47:55 -0700

    InsertInterval

--- src/include/tests/LC/InsertInterval.h
+++ src/include/tests/LC/InsertInterval.h
@@ -20,97 +20,117 @@ This is because the new interval [4,9] overlaps with [3,5],[6,7],[8,10].
 
 namespace InsertInterval
 {
-  // Definition for an interval.
-  struct Interval {
-    int start;
-    int end;
-    Interval() : start(0), end(0) {}
-    Interval(int s, int e) : start(s), end(e) {}
-  };
-
-  vector<Interval> insert(vector<Interval>& intervals, Interval newInterval)
-  {
-    int n = intervals.size();
-    if (0 == n) return vector<Interval>{ newInterval };
-    vector<Interval> result;
-    for (int i = 0; i < n; i++)
+    // Definition for an interval.
+    struct Interval {
+        int start;
+        int end;
+        Interval() : start(0), end(0) {}
+        Interval(int s, int e) : start(s), end(e) {}
+    };
+    
+    vector<Interval> insert_1(vector<Interval>& intervals, Interval newInterval)
     {
-      auto& interval = intervals[i];
-      if (newInterval.start > interval.end)
-      {
-        // newInterval comes after interval
-        result.emplace_back(interval);
-        continue;
-      }
-
-      if (newInterval.end < interval.start)
-      {
-        // newInterval comes before interval
-        // add newInterval, and then the rest of intervals
+        int n = intervals.size();
+        if (0 == n) return vector<Interval>{ newInterval };
+        vector<Interval> result;
+        for (int i = 0; i < n; i++)
+        {
+            auto& interval = intervals[i];
+            if (newInterval.start > interval.end)
+            {
+                // newInterval comes after interval
+                result.emplace_back(interval);
+                continue;
+            }
+            
+            if (newInterval.end < interval.start)
+            {
+                // newInterval comes before interval
+                // add newInterval, and then the rest of intervals
+                result.emplace_back(newInterval);
+                result.insert(result.end(), intervals.begin() + i, intervals.end());
+                return result;
+            }
+            // there is overlapping, merge interval with newInterval
+            newInterval.start = min(newInterval.start, interval.start);
+            newInterval.end = max(newInterval.end, interval.end);
+        }
         result.emplace_back(newInterval);
-        result.insert(result.end(), intervals.begin() + i, intervals.end());
         return result;
-      }
-      // there is overlapping, merge interval with newInterval
-      newInterval.start = min(newInterval.start, interval.start); 
-      newInterval.end = max(newInterval.end, interval.end);
     }
-    result.emplace_back(newInterval);
-    return result;
-  }
-
-  //static bool cmp(const Interval &interval1, const Interval &interval2)
-  //{
-  //  return interval1.start < interval2.start;
-  //}
-
-  vector<Interval> merge(vector<Interval>& intervals)
-  {
-    vector<Interval> result;
-    int n = intervals.size();
-    if (n == 0) return result;
-	sort(intervals.begin(), intervals.end(), [](const Interval& int1, const Interval& int2) { return int1.start < int2.start; });
-    for (int i = 0; i < n; i++)
+    
+    //static bool cmp(const Interval &interval1, const Interval &interval2)
+    //{
+    //  return interval1.start < interval2.start;
+    //}
+    
+    vector<Interval> merge(vector<Interval>& intervals)
     {
-      auto start = intervals[i].start;
-      auto end = intervals[i].end;
-      while (i < n - 1 && end >= intervals[i + 1].start)
-      {
-        if (intervals[i + 1].end > end)
-          end = intervals[i + 1].end;
-        i++;
-      }
-      result.emplace_back(start, end);
+        vector<Interval> result;
+        int n = intervals.size();
+        if (n == 0) return result;
+        sort(intervals.begin(), intervals.end(), [](const Interval& int1, const Interval& int2) { return int1.start < int2.start; });
+        for (int i = 0; i < n; i++)
+        {
+            auto start = intervals[i].start;
+            auto end = intervals[i].end;
+            while (i < n - 1 && end >= intervals[i + 1].start)
+            {
+                if (intervals[i + 1].end > end)
+                    end = intervals[i + 1].end;
+                i++;
+            }
+            result.emplace_back(start, end);
+        }
+        return result;
     }
-    return result;
-  }
-
-  vector<Interval> insertWithMerge(vector<Interval>& intervals, Interval newInterval)
-  {
-    intervals.emplace_back(newInterval);
-    return merge(intervals);
-  }
-
-  int Test(vector<pair<int, int>> intPairs)
-  {
-    vector<Interval> intervals;
-    for (const auto& pair : intPairs)
+    
+    vector<Interval> insert_2(vector<Interval>& intervals, Interval newInterval)
     {
-      cout << "[" << pair.first << "," << pair.second << "] ";
-      intervals.emplace_back(pair.first, pair.second);
+        intervals.emplace_back(newInterval);
+        return merge(intervals);
     }
-    cout << endl;
-
-    Interval newInterval(2, 6);
-    cout << "[" << newInterval.start << "," << newInterval.end << "] ";
-    cout << endl;
-
-    auto result = insert(intervals, newInterval);
-    for (const auto& interval : result)
+    
+    vector<Interval> insert(vector<Interval>& intervals, Interval newInterval) {
+        auto lesser = [](const Interval& first, const Interval& second) {
+            return first.end < second.start;
+        };
+        auto equalRange = equal_range(intervals.begin(), intervals.end(), newInterval, lesser);
+        auto it1 = equalRange.first;
+        auto it2 = equalRange.second;
+        // no overlapping
+        if (it1 == it2) {
+            intervals.insert(it1, newInterval);
+        } else {
+            it2--;
+            it1->start = min(it1->start, newInterval.start);
+            it1->end = max(it2->end, newInterval.end);
+            intervals.erase(it1 + 1, it2 + 1);
+        }
+        return intervals;
+    }
+    
+    int Test()
     {
-      cout << "[" << interval.start << "," << interval.end << "] ";
+        vector<pair<int, int>> intPairs = { { 1,3 }, { 6,9 }, { 10, 12 } };
+        vector<Interval> intervals;
+        for (const auto& pair : intPairs)
+        {
+            cout << "[" << pair.first << "," << pair.second << "] ";
+            intervals.emplace_back(pair.first, pair.second);
+        }
+        cout << endl;
+        
+        Interval newInterval(4, 15);
+        cout << "[" << newInterval.start << "," << newInterval.end << "] ";
+        cout << endl;
+        
+        auto result = insert(intervals, newInterval);
+        for (const auto& interval : result)
+        {
+            cout << "[" << interval.start << "," << interval.end << "] ";
+        }
+        cout << endl;
+        return 0;
     }
-    cout << endl;
-    return 0;
-  }
 }
