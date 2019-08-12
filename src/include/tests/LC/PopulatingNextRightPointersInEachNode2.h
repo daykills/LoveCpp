e26f53439a7cdc947667aafc86c263d57e7e0116
ZJ Yan <zengjia_yan@apple.com>
2019-08-11 18:50:35 -0700

    PopulatingNextRightPointersInEachNode2

--- src/include/tests/LC/PopulatingNextRightPointersInEachNode2.h
+++ src/include/tests/LC/PopulatingNextRightPointersInEachNode2.h
@@ -31,68 +31,76 @@ After calling your function, the tree should look like:
 
 namespace PopulatingNextRightPointersInEachNode2
 {
-	// Definition for a binary tree node.
-	struct TreeLinkNode {
-		int val;
-		TreeLinkNode *left, *right, *next;
-		TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
-	};
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+    
+    Node() {}
+    
+    Node(int _val, Node* _left, Node* _right, Node* _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
 
-	void connect(TreeLinkNode *root)
-	{
-		while (root)
-		{
-			auto head = root;
-			// set root as null, and set it to be the first child found later
-			root = nullptr;
-			TreeLinkNode* lastChildNode = nullptr;
-			while (head)
-			{
-				vector<TreeLinkNode*> children{ head->left, head->right };
-				for (auto child : children)
-				{
-					if (child != nullptr)
-					{
-						// if the last ChildNode is not set yet
-						if (lastChildNode == nullptr)
-						{
-							lastChildNode = child;
-							// this child will become the root of next iteration
-							root = child;
-							continue;
-						}
-						// link the lastChildNode with current one
-						lastChildNode->next = child;
-						lastChildNode = child;
-					}
-				}
-				head = head->next;
-			}
-		}
-	}
-	////////////////////////////////////////////////////////////////////////////////////
-	static void Test()
-	{
-		//[1, 2, 2, 3, 3, null, null, 4, 4]
-		const int n = 7;
-		TreeLinkNode* nodes[n];
-
-		for (int i = 0; i < n; i++)
-		{
-			srand(i);
-			nodes[i] = new TreeLinkNode(rand() % 23);
-		}
-		nodes[0]->left = nodes[1];
-		nodes[0]->right = nodes[2];
-		nodes[1]->left = nodes[3];
-		nodes[1]->right = nodes[4];
-		nodes[3]->left = nodes[5];
-		nodes[3]->right = nodes[6];
-
-		TreePrinter::Printer<TreeLinkNode> treePrinter;
-		treePrinter.printPretty(nodes[0], 1, 1, cout);
-
-		connect(nodes[0]);
-	}
+Node* connect(Node* root) {
+    auto result = root;
+    Node* firstOfLevel = nullptr;
+    Node* preNode = nullptr;
+    auto updateNodes = [&firstOfLevel, &preNode](Node* child) {
+        if (!child) return;
+        if (!firstOfLevel) {
+            firstOfLevel = child;
+        }
+        if (preNode) {
+            preNode->next = child;
+        }
+        preNode = child;
+    };
+    while (root) {
+        auto parent = root;
+        firstOfLevel = nullptr;
+        preNode = nullptr;
+        while (parent) {
+            updateNodes(parent->left);
+            updateNodes(parent->right);
+            parent = parent->next;
+        }
+        root = firstOfLevel;
+    }
+    return result;
+}
+////////////////////////////////////////////////////////////////////////////////////
+static void Test()
+{
+    //[1, 2, 2, 3, 3, null, null, 4, 4]
+    /*
+    const int n = 7;
+    Node* nodes[n];
+    
+    for (int i = 0; i < n; i++)
+    {
+        srand(i);
+        nodes[i] = new Node();
+    }
+    nodes[0]->left = nodes[1];
+    nodes[0]->right = nodes[2];
+    nodes[1]->left = nodes[3];
+    nodes[1]->right = nodes[4];
+    nodes[3]->left = nodes[5];
+    nodes[3]->right = nodes[6];
+    
+    TreePrinter::Printer<TreeLinkNode> treePrinter;
+    treePrinter.printPretty(nodes[0], 1, 1, cout);
+    
+    connect(nodes[0]);
+     */
+}
 }
 
