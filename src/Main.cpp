324fa9d48004d732b66b3a2a56921b4439570fb6
ZJ Yan <zengjia_yan@apple.com>
2019-08-27 11:41:04 -0700

    DecodeString

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,7 +3,7 @@
 
 #include "Common.h"
 
-#include "tests/LC/MaxPointsOnALine.h"
+#include "tests/LC/DecodeString.h"
 
 #include <iostream>
 #include <memory>
@@ -14,7 +14,7 @@ int main(int argc, char* argv[])
 	auto begin = chrono::high_resolution_clock::now();
 
 	// code to benchmark
-    MaxPointsOnALine::Test();
+    DecodeString::Test();
 
     std::cout << "Total time lapsed: " << chrono::duration_cast<chrono::nanoseconds>(chrono::high_resolution_clock::now() - begin).count() * 0.001 << "ms" << std::endl;
 	return 0;
