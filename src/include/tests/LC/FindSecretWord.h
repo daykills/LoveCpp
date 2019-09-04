53f0815da60412f4f98c240e754210fa51889324
ZJ Yan <zengjia_yan@apple.com>
2019-09-04 08:53:09 -0700

    FindSecretWord.h

---
+++ src/include/tests/LC/FindSecretWord.h
@@ -0,0 +1,92 @@
+/*
+ 843. Guess the Word
+
+ This problem is an interactive problem new to the LeetCode platform.
+ 
+ We are given a word list of unique words, each word is 6 letters long, and one word in this list is chosen as secret.
+ 
+ You may call master.guess(word) to guess a word.  The guessed word should have type string and must be from the original list with 6 lowercase letters.
+ 
+ This function returns an integer type, representing the number of exact matches (value and position) of your guess to the secret word.  Also, if your guess is not in the given wordlist, it will return -1 instead.
+ 
+ For each test case, you have 10 guesses to guess the word. At the end of any number of calls, if you have made 10 or less calls to master.guess and at least one of these guesses was the secret, you pass the testcase.
+ 
+ Besides the example test case below, there will be 5 additional test cases, each with 100 words in the word list.  The letters of each word in those testcases were chosen independently at random from 'a' to 'z', such that every word in the given word lists is unique.
+ 
+ Example 1:
+ Input: secret = "acckzz", wordlist = ["acckzz","ccbazz","eiowzz","abcczz"]
+ 
+ Explanation:
+ 
+ master.guess("aaaaaa") returns -1, because "aaaaaa" is not in wordlist.
+ master.guess("acckzz") returns 6, because "acckzz" is secret and has all 6 matches.
+ master.guess("ccbazz") returns 3, because "ccbazz" has 3 matches.
+ master.guess("eiowzz") returns 2, because "eiowzz" has 2 matches.
+ master.guess("abcczz") returns 4, because "abcczz" has 4 matches.
+ 
+ We made 5 calls to master.guess and one of them was the secret, so we pass the test case.
+ Note:  Any solutions that attempt to circumvent the judge will result in disqualification.
+ */
+
+#include <iostream>
+#include <list>
+#include "Common.h"
+
+namespace FindSecretWord
+{
+class Master {
+public:
+    Master(const string& secret, vector<string>& wordlist) : m_secret(secret), m_wordset(wordlist.begin(), wordlist.end()) {}
+    int guess(const string& word) const {
+        if (m_wordset.count(word) == 0)
+            return -1;
+        assert(m_secret.size() == word.size());
+        int cnt = 0;
+        for (auto i = 0; i < word.size(); i++) {
+            if (m_secret[i] == word[i])
+                cnt++;
+        }
+        return cnt;
+    }
+private:
+    unordered_set<string> m_wordset;
+    string m_secret;
+};
+int commonCharCount(const string& base, const string& str) {
+    assert(base.size() == str.size());
+    int cnt = 0;
+    for (auto i = 0; i < base.size(); i++) {
+        if (base[i] == str[i])
+            cnt++;
+    }
+    return cnt;
+}
+void findSecretWord(vector<string>& wordlist, Master& master) {
+    unordered_set<string> dropSet;
+    for (auto& word : wordlist) {
+        if (dropSet.count(word))
+            continue;
+        dropSet.emplace(word);
+        auto match = master.guess(word);
+        if (match == 0) continue;
+        
+        assert(match != -1);
+        if (match == word.size())
+            return;
+        for (auto& str : wordlist) {
+            if (str == word || dropSet.count(str))
+                continue;
+            if (commonCharCount(word, str) != match)
+                dropSet.emplace(str);
+        }
+    }
+}
+
+static void Test()
+{
+    string secret = "ccoyyo";
+    vector<string> wordlist = {"wichbx","oahwep","tpulot","eqznzs","vvmplb","eywinm","dqefpt","kmjmxr","ihkovg","trbzyb","xqulhc","bcsbfw","rwzslk","abpjhw","mpubps","viyzbc","kodlta","ckfzjh","phuepp","rokoro","nxcwmo","awvqlr","uooeon","hhfuzz","sajxgr","oxgaix","fnugyu","lkxwru","mhtrvb","xxonmg","tqxlbr","euxtzg","tjwvad","uslult","rtjosi","hsygda","vyuica","mbnagm","uinqur","pikenp","szgupv","qpxmsw","vunxdn","jahhfn","kmbeok","biywow","yvgwho","hwzodo","loffxk","xavzqd","vwzpfe","uairjw","itufkt","kaklud","jjinfa","kqbttl","zocgux","ucwjig","meesxb","uysfyc","kdfvtw","vizxrv","rpbdjh","wynohw","lhqxvx","kaadty","dxxwut","vjtskm","yrdswc","byzjxm","jeomdc","saevda","himevi","ydltnu","wrrpoc","khuopg","ooxarg","vcvfry","thaawc","bssybb","ccoyyo","ajcwbj","arwfnl","nafmtm","xoaumd","vbejda","kaefne","swcrkh","reeyhj","vmcwaf","chxitv","qkwjna","vklpkp","xfnayl","ktgmfn","xrmzzm","fgtuki","zcffuv","srxuus","pydgmq"};
+    Master master(secret, wordlist);
+    findSecretWord(wordlist, master);
+}
+}
---
+++ src/include/tests/LC/NumMatrix.h
@@ -0,0 +1,97 @@
+/*
+304. Range Sum Query 2D - Immutable
+
+Given a 2D matrix matrix, find the sum of the elements inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
+
+Range Sum Query 2D
+The above rectangle (with the red border) is defined by (row1, col1) = (2, 1) and (row2, col2) = (4, 3), which contains sum = 8.
+
+Example:
+
+Given matrix = [
+  [3, 0, 1, 4, 2],
+  [5, 6, 3, 2, 1],
+  [1, 2, 0, 1, 5],
+  [4, 1, 0, 1, 7],
+  [1, 0, 3, 0, 5]
+]
+
+sumRegion(2, 1, 4, 3) -> 8
+sumRegion(1, 1, 2, 2) -> 11
+sumRegion(1, 2, 2, 4) -> 12
+Note:
+
+You may assume that the matrix does not change.
+There are many calls to sumRegion function.
+You may assume that row1 ≤ row2 and col1 ≤ col2.
+ */
+
+#include <iostream>
+#include <list>
+#include "Common.h"
+
+namespace NumMatrix
+{
+
+static vector<vector<int>> s_dummy;
+
+class NumMatrix {
+public:
+    NumMatrix(vector<vector<int>>& matrix) : m_matrix(matrix.empty() ? s_dummy : matrix) {
+        preprocess();
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        auto m = m_matrix.size();
+        if (m == 0)
+            return 0;
+        auto n = m_matrix[0].size();
+        if (n == 0)
+            return 0;
+        assert(row1 <= row2 && col1 <= col2);
+        auto sum = m_matrix[row2][col2];
+        if (row1 == 0 && col1 == 0)
+            return sum;
+        if (row1 > 0 && col1 > 0)
+            sum += m_matrix[row1 - 1][col1 - 1] - m_matrix[row2][col1 - 1] - m_matrix[row1 - 1][col2];
+        else if (row1 == 0)
+            sum -= m_matrix[row2][col1 - 1];
+        else // col1 == 0
+            sum -= m_matrix[row1 - 1][col2];
+        return sum;
+    }
+private:
+    void preprocess() {
+        auto m = m_matrix.size();
+        if (m == 0)
+            return;
+        auto n = m_matrix[0].size();
+        if (n == 0)
+            return;
+        vector<int> buff;
+        for (auto i = 0; i < m; i++) {
+            auto sumInRow = 0;
+            for (auto j = 0; j < n; j++) {
+                sumInRow += m_matrix[i][j];
+                m_matrix[i][j] = sumInRow;
+                if (i > 0) m_matrix[i][j] += m_matrix[i - 1][j];
+            }
+        }
+    }
+    vector<vector<int>>& m_matrix;
+};
+
+static void Test()
+{
+    vector<vector<int>> matrix = {
+        {3,0,1,4,2},
+        {5,6,3,2,1},
+        {1,2,0,1,5},
+        {4,1,0,1,7},
+        {1,0,3,0,5}};
+    NumMatrix* obj = new NumMatrix(matrix);
+    std::cout << obj->sumRegion(2,1,4,3) << std::endl;
+    std::cout << obj->sumRegion(1,1,2,2) << std::endl;
+    std::cout << obj->sumRegion(1,2,2,4) << std::endl;
+}
+}
