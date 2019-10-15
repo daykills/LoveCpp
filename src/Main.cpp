be872d4fe53ee409ec247f0cf11e42b332e72009
ZJ Yan <zengjia_yan@apple.com>
2019-10-15 08:57:41 -0700

    SolveEquation

--- src/Main.cpp
+++ src/Main.cpp
@@ -7,7 +7,7 @@
 #define STR(x) XSTR(x)
 
 #define DIR tests/LC
-#define TEST_NAME MaxAreaOfIsland
+#define TEST_NAME SolveEquation
 #include STR(DIR/TEST_NAME.h)
 
 #include <iostream>
--- src/include/tests/LC/SolveEquation.h
+++ src/include/tests/LC/SolveEquation.h
@@ -42,9 +42,35 @@ void parse(string expr, int& coef, int& val) {
     assert(!expr.empty());
     // add "+" to make expression formalized
     if (expr[0] != '-') expr = "+" + expr;
-    for (auto ch : expr) {
-        
+    
+    val = 0;
+    coef = 0;
+    int sign = 1;
+    int curNum = 0;
+    for (auto i = 0; i < expr.size(); i++) {
+        if (expr[i] == '-' || expr[i] == '+') {
+            // commit previous segment without x
+            if (i > 0) {
+                if (expr[i - 1] != 'x') {
+                    val += curNum * sign;
+                }
+                curNum = 0;
+            }
+            sign = expr[i] == '-' ? -1 : 1;
+        } else if (expr[i] == 'x') {
+            assert(i > 0);
+            if (expr[i - 1] <= '9' && expr[i - 1] >= '0') {
+                coef += curNum * sign;
+            } else {
+                coef += sign;
+            }
+            curNum = 0;
+        } else {
+            assert(expr[i] <= '9' && expr[i] >= '0');
+            curNum = curNum * 10 + expr[i] - '0';
+        }
     }
+    val += curNum * sign;
 }
 
 string solveEquation(string equation) {
@@ -56,11 +82,19 @@ string solveEquation(string equation) {
     parse(left, lCoef, lVal);
     int rCoef, rVal;
     parse(right, rCoef, rVal);
-    return "";
+    int coef = lCoef - rCoef;
+    int val = rVal - lVal;
+    if (coef == 0 && val == 0)
+        return "Infinite solutions";
+    if (coef == 0)
+        return "No solution";
+    if (val == 0)
+        return "x=0";
+    return "x=" + to_string(val / coef);
 }
 
 void Test()
 {
-    cout << solveEquation("2x+3x-6x=x+2") << endl;
+    cout << solveEquation("x+5-3+x=6+x-2") << endl;
 }
 }
