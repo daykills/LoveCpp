162b1122aee5d395b3656b0e839f54cc894a61e5
ZJ Yan <zengjia_yan@apple.com>
2019-12-29 23:53:39 -0800

    update

--- src/Main.cpp
+++ src/Main.cpp
@@ -7,7 +7,7 @@
 #define STR(x) XSTR(x)
 
 #define DIR tests/LC
-#define TEST_NAME InsertInterval
+#define TEST_NAME MergeKSortedLists
 #include STR(DIR/TEST_NAME.h)
 
 #include <iostream>
--- src/include/tests/LC/MergeKSortedLists.h
+++ src/include/tests/LC/MergeKSortedLists.h
@@ -1,126 +1,90 @@
 /*
 23. Merge k Sorted Lists
-Total Accepted: 72642 Total Submissions: 322524 Difficulty: Hard
-Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+ Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+ 
+ Example:
+ 
+ Input:
+ [
+ 1->4->5,
+ 1->3->4,
+ 2->6
+ ]
+ Output: 1->1->2->3->4->4->5->6
 */
 namespace MergeKSortedLists
 {
-  // Definition for singly-linked list.
-  struct ListNode {
+// Definition for singly-linked list.
+struct ListNode {
     int val;
     ListNode *next;
     ListNode(int x) : val(x), next(nullptr) {}
-  };
+};
 
-  // function to merge two sorted lists
-  ListNode* mergeLists(ListNode* list1, ListNode* list2)
-  {
-    if (list1 == nullptr) return list2;
-
-    ListNode dummy(-1);
-    ListNode* preNode = &dummy;
-
-    while (list1 != nullptr && list2 != nullptr)
-    {
-      // pick nodes from the smaller ones
-      if (list1->val <= list2->val)
-      {
-        preNode->next = list1;
-        list1 = list1->next;
-      }
-      else
-      {
-        preNode->next = list2;
-        list2 = list2->next;
-      }
-      preNode = preNode->next;
+ListNode* mergeKLists(vector<ListNode*>& lists) {
+    
+    auto lessor = [](const ListNode* lhs, const ListNode* rhs) {
+        return lhs->val > rhs->val;
+    };
+    // top is has the smallest root node
+    priority_queue<ListNode*, vector<ListNode*>, decltype(lessor)> minHeap(lessor);
+    while (!lists.empty()) {
+        if (lists.back())
+            minHeap.emplace(lists.back());
+        lists.pop_back();
     }
-    preNode->next = (list1 == nullptr) ? list2 : list1;
+    if (minHeap.empty()) return nullptr;
+    
+    ListNode dummy(0);
+    auto pre = &dummy;
+    // get the smallest node one by one
+    while (!minHeap.empty()) {
+        pre->next = minHeap.top();
+        minHeap.pop();
+        auto next = pre->next->next;
+        if (next)
+            minHeap.push(next);
+        pre = pre->next;
+    }
+    pre->next = nullptr;
     return dummy.next;
-  }
-  ListNode* mergeKLists(vector<ListNode*>& lists)
-  {
-    // when n <= 1
-    int n = lists.size();
-    if (n == 0) return nullptr;
-    if (n == 1) return lists[0];
+}
 
-    // job queue
-    queue<ListNode*> jobQ;
-    // add all lists to the job queue
-    for (auto list : lists)
-    {
-      jobQ.push(list);
+static ListNode* Test()
+{
+    vector<vector<int>> nodes = {
+        {1},
+        {0},
+    };
+    vector<ListNode*> lists;
+    for (auto& values : nodes) {
+        lists.emplace_back();
+        if (values.empty())
+            continue;
+        auto root = new ListNode(values.front());
+        auto pre = root;
+        for (auto i = 1; i < values.size(); i++) {
+            pre->next = new ListNode(values[i]);
+            pre = pre->next;
+        }
+        lists.back() = root;
     }
-    while (true)
-    {
-      auto list1 = jobQ.front();
-      jobQ.pop();
-      auto list2 = jobQ.front();
-      jobQ.pop();
-      auto merged = mergeLists(list1, list2);
-      if (jobQ.empty())
-      {
-        return merged;
-      }
-      else
-      {
-        // add the job to the back of the queue, so that we don't have one too-long list
-        jobQ.push(merged);
-      }
+    for (auto list : lists) {
+        while (list) {
+            cout << list->val << " -> ";
+            list = list->next;
+        }
+        cout << endl;
     }
-  }
-
-  static ListNode* Test()
-  {
-    const int n = 7;
-    ListNode* nodes[n];
-    for (int i = n - 1; i >= 0; i--)
-    {
-      nodes[i] = new ListNode(i * i);
-      nodes[i]->next = i == (n - 1) ? nullptr : nodes[i + 1];
-    }
-
-    ListNode* result = nodes[0];
-    int i = 0;
-    while (result)
-    {
-      cout << "i = " << i << " cur = " << result << " label = " << result->val << " next = " << result->next << endl;
-      i++;
-      result = result->next;
-    }
-
     cout << "***********" << endl;
-
-    const int m = 5;
-    ListNode* nodes2[m];
-    for (i = m - 1; i >= 0; i--)
-    {
-      nodes2[i] = new ListNode(4 * i + 5);
-      nodes2[i]->next = i == (m - 1) ? nullptr : nodes2[i + 1];
+    
+    auto result = mergeKLists(lists);
+    while (result) {
+        cout << result->val << " -> ";
+        result = result->next;
     }
-    result = nodes2[0];
-    i = 0;
-    while (result)
-    {
-      cout << "i = " << i << " cur = " << result << " label = " << result->val << " next = " << result->next << endl;
-      i++;
-      result = result->next;
-    }
-
-    cout << "***********" << endl;
-    vector<ListNode*> sortedLists(2);
-    sortedLists[0] = nodes[0];
-    sortedLists[1] = nodes2[0];
-    result = mergeKLists(sortedLists);
-    i = 0;
-    while (result)
-    {
-      cout << "i = " << i << " cur = " << result << " label = " << result->val << " next = " << result->next << endl;
-      i++;
-      result = result->next;
-    }
-
+    cout << endl;
+    
     return 0;
-  }
+}
 }
