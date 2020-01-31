89a8184299ab3dfc70c25be9ce3e8c8f250ee59d
ZJ Yan <zengjia_yan@apple.com>
2020-01-31 11:07:35 -0800

    update all O1

--- src/include/tests/LC/AllO1DataStructure.h
+++ src/include/tests/LC/AllO1DataStructure.h
@@ -21,89 +21,82 @@ class AllOne {
 public:
     /** Initialize your data structure here. */
     AllOne() {}
-    
     /** Inserts a new key <Key> with value 1. Or increments an existing key by 1. */
     void inc(string key) {
-        if (m_keyMap.count(key)) {
-            auto bucketIter = m_keyMap[key];
-            auto val = bucketIter->val + 1;
-            bucketIter->keys.erase(key);
-            auto nextIter = std::next(bucketIter);
-            // if already last bucket
-            if (nextIter == m_buckets.end()) {
-                nextIter = m_buckets.insert(m_buckets.end(), { val, { key } });
-            } else if (nextIter->val == val) {
-                nextIter->keys.emplace(key);
+        if (m_keyMap.count(key) == 0) {
+            // update bucket
+            if (m_buckets.empty() || m_buckets.front().val > 1) {
+                m_buckets.insert(m_buckets.begin(), { 1, { key } });
             } else {
-                // new bucket
-                nextIter = m_buckets.insert(nextIter, { val, { key } });
-            }
-            m_keyMap[key] = nextIter;
-            if (bucketIter->keys.empty()) {
-                m_buckets.erase(bucketIter);
-            }
-        } else {
-            // new bucket with val 1
-            if (!m_buckets.empty() && m_buckets.front().val == 1) {
                 m_buckets.front().keys.emplace(key);
-            } else {
-                m_buckets.push_front({ 1, { key } });
             }
+            // update key map
             m_keyMap[key] = m_buckets.begin();
+            return;
         }
+        // update bucket
+        auto itBucket = m_keyMap[key];
+        itBucket->keys.erase(key);
+        
+        auto itNext = next(itBucket);
+        if (itNext == m_buckets.end() || itNext->val > itBucket->val + 1) {
+            // insert new node
+            m_buckets.insert(itNext, { itBucket->val + 1, { key } });
+        } else {
+            // add key to next iterator
+            itNext->keys.emplace(key);
+        }
+        // update keyMap
+        m_keyMap[key] = next(itBucket);
+        if (itBucket->keys.empty())
+            m_buckets.erase(itBucket);
     }
     
     /** Decrements an existing key by 1. If Key's value is 1, remove it from the data structure. */
     void dec(string key) {
-        if (m_buckets.empty())
+        if (m_keyMap.count(key) == 0)
             return;
-        if (m_keyMap.count(key)) {
-            auto bucketIter = m_keyMap[key];
-            auto val = bucketIter->val - 1;
-            bucketIter->keys.erase(key);
+        auto itBucket = m_keyMap[key];
+        // erase key
+        if (itBucket->val == 1) {
+            itBucket->keys.erase(key);
+            if (itBucket->keys.empty())
+                m_buckets.erase(itBucket);
             m_keyMap.erase(key);
-            if (val > 0) {
-                if (bucketIter == m_buckets.begin()) {
-                    m_buckets.push_front( {val - 1, { key } });
-                    m_keyMap[key] = m_buckets.begin();
-                } else {
-                    auto preIter = prev(bucketIter);
-                    if (preIter->val == val) {
-                        preIter->keys.emplace(key);
-                        m_keyMap[key] = preIter;
-                    } else {
-                        auto newIter = m_buckets.insert(bucketIter, { val, { key } });
-                        m_keyMap[key] = newIter;
-                        bucketIter->keys.erase(key);
-                    }
-                }
-            }
-            if (bucketIter->keys.empty()) {
-                m_buckets.erase(bucketIter);
-            }
+            return;
         }
+        
+        if (itBucket == m_buckets.begin() || prev(itBucket)->val < itBucket->val - 1) {
+            m_buckets.insert(itBucket, { itBucket->val - 1, { key } });
+        } else {
+            prev(itBucket)->keys.emplace(key);
+        }
+        itBucket->keys.erase(key);
+        m_keyMap[key] = prev(itBucket);
+        if (itBucket->keys.empty())
+            m_buckets.erase(itBucket);
     }
     
     /** Returns one of the keys with maximal value. */
     string getMaxKey() {
-        if (m_buckets.empty())
-            return "";
+        if (m_buckets.empty()) return "";
         return *m_buckets.back().keys.begin();
     }
     
     /** Returns one of the keys with Minimal value. */
     string getMinKey() {
-        if (m_buckets.empty())
-            return "";
+        if (m_buckets.empty()) return "";
         return *m_buckets.front().keys.begin();
     }
 private:
+    // bucket to keep all the keys with the same value
     struct Bucket { int val; unordered_set<string> keys; };
-    using BucketList = std::list<Bucket>;
+    using BucketList = list<Bucket>;
     BucketList m_buckets;
-    // key -> the list iterator
+    // key map keeps key location
     unordered_map<string, BucketList::iterator> m_keyMap;
 };
+
 
 static void Test()
 {
@@ -130,6 +123,7 @@ static void Test()
     
     // ["AllOne","inc","inc","inc","inc","inc","dec","dec","getMaxKey","getMinKey"]
     // [[],["a"],["b"],["b"],["b"],["b"],["b"],["b"],[],[]]
+    /*
     AllOne inst;
     inst.inc("a");
     inst.inc("b");
@@ -140,6 +134,25 @@ static void Test()
     std::cout << "getMinKey: " << inst.getMinKey() << std::endl;
     inst.dec("b");
     inst.dec("b");
+    std::cout << "getMaxKey: " << inst.getMaxKey() << std::endl;
+    std::cout << "getMinKey: " << inst.getMinKey() << std::endl;
+    */
+    // ["AllOne","inc","inc","inc","inc","inc","inc","inc","inc","inc","inc","inc","inc","getMinKey"]
+    // [[],["a"],["b"],["c"],["d"],["a"],["b"],["c"],["d"],["c"],["d"],["d"],["a"],[]]
+    AllOne inst;
+    inst.inc("a");
+    inst.inc("b");
+    inst.inc("c");
+    inst.inc("d");
+    inst.inc("a");
+    inst.inc("b");
+    inst.inc("c");
+    inst.inc("d");
+    inst.inc("c");
+    inst.inc("d");
+    inst.inc("d");
+    inst.inc("a");
+    
     std::cout << "getMaxKey: " << inst.getMaxKey() << std::endl;
     std::cout << "getMinKey: " << inst.getMinKey() << std::endl;
 }
