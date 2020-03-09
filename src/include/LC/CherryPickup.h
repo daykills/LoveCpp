f49da8a67b029d8bf8811b637105005ce02b3066
ZJ Yan <zengjia_yan@apple.com>
2020-03-08 21:50:00 -0700

    dp

--- src/include/LC/CherryPickup.h
+++ src/include/LC/CherryPickup.h
@@ -48,23 +48,28 @@
 namespace CherryPickup
 {
 // max cherries to be picked up by two ppl starting r1, c1, r2, c2
-int dfs(vector<vector<int>>& grid, int r1, int c1, int r2, int c2) {
+int dfs(vector<vector<int>>& grid, int r1, int c1, int r2,
+        vector<vector<vector<int>>>& dp) {
     auto n = grid.size();
     assert(n == grid[0].size());
+    
+    int c2 = r1 + c1 - r2;
     // only need to check upper bound
     if (r1 >= n || c1 >= n || r2 >= n || c2 >= n
         || grid[r1][c1] == - 1 || grid[r2][c2] == - 1)
         return INT_MIN;
+    
+    if (dp[r1][c1][r2] != 0)
+        return dp[r1][c1][r2];
     
     int tmp1 = grid[r1][c1];
     int tmp2 = grid[r2][c2];
     // when one person reaches end, we return as the other one has to reach the end as well - they have same steps
     // we could return INT_MIN if the other person is not at end, but not necessary
     if (r1 == n - 1 && c1 == n - 1)
-        return tmp1;
+        return r2 == r1 ? tmp1 : INT_MIN;
     if (r2 == n - 1 && c2 == n - 1)
-        return tmp2;
-
+        return r2 == r1 ? tmp2 : INT_MIN;
     
     int cherry = 0;
     // reach the same spot, only one gets cherry
@@ -76,34 +81,44 @@ int dfs(vector<vector<int>>& grid, int r1, int c1, int r2, int c2) {
     // mark them picked
     grid[r1][c1] = grid[r2][c2] = 0;
     
-    int moreCherry = 0;
-    moreCherry = max(moreCherry, dfs(grid, r1 + 1, c1, r2 + 1, c2));
-    moreCherry = max(moreCherry, dfs(grid, r1 + 1, c1, r2, c2 + 1));
-    moreCherry = max(moreCherry, dfs(grid, r1, c1 + 1, r2 + 1, c2));
-    moreCherry = max(moreCherry, dfs(grid, r1, c1 + 1, r2, c2 + 1));
+    int moreCherry = INT_MIN;
+    moreCherry = max(moreCherry, dfs(grid, r1 + 1, c1, r2 + 1, dp));
+    moreCherry = max(moreCherry, dfs(grid, r1 + 1, c1, r2, dp));
+    moreCherry = max(moreCherry, dfs(grid, r1, c1 + 1, r2 + 1, dp));
+    moreCherry = max(moreCherry, dfs(grid, r1, c1 + 1, r2, dp));
     grid[r1][c1] = tmp1;
     grid[r2][c2] = tmp2;
-    return cherry + moreCherry;
+    
+    dp[r1][c1][r2] = cherry + moreCherry;
+    return dp[r1][c1][r2];
 }
 
-int cherryPickupDFS(vector<vector<int>>& grid) {
+int cherryPickup(vector<vector<int>>& grid) {
     auto n = grid.size();
     assert(n == grid[0].size());
     
-    int maxCherry = dfs(grid, 0, 0, 0, 0);
-    return maxCherry;
+    vector<vector<vector<int>>> dp(n, vector<vector<int>>(n, vector<int>(n, 0)));
+    
+    int maxCherry = dfs(grid, 0, 0, 0, dp);
+    return max(0, maxCherry);
 }
 
 void Test()
 {
     vector<vector<int>> grid {
         /*
+        {1,1,-1},
+        {1,-1,1},
+        {-1,1,1}
+        */
+        /*
         {1,1,1,0,0},
         {0,0,1,0,1},
         {1,0,1,0,0},
         {0,0,1,0,0},
         {0,0,1,1,1}
-         */
+        */
+        
         {1,1,1,1,-1,-1,-1,1,0,0},
         {1,0,0,0,1,0,0,0,1,0},
         {0,0,1,1,1,1,0,1,1,1},
@@ -114,13 +129,14 @@ void Test()
         {1,-1,0,1,0,0,0,1,-1,1},
         {1,0,-1,0,-1,0,0,1,0,0},
         {0,0,-1,0,1,0,1,0,0,1}
+        
         /*
         {1,1,1},
         {1,1,0},
         {0,1,1},
          */
     };
-    cout << "result: " << cherryPickupDFS(grid) << endl;
+    cout << "result: " << cherryPickup(grid) << endl;
 }
 
 }
